5fd578da59630d0d4b89f33ac4ab31da137149ed
Nikku <yoks.rar@gmail.com>
2024-04-19 20:36:47 -0400

    Header y colores

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+    <header>	 
+		<div class="portada">   
+			<h1> ◉ ROLEPLAYER ACCOUNT </h1>    
+		</div>
+		<div class="menu"> 
+			<div class="grid-menu">  
+				 <a href="# ">INICIO </a> 
+				 <a href="# ">PERSONAJES</a>   
+				 <a href="# ">COMBATES</a> 
+				 <a href="# ">TROFEOS</a> 
+			</div>	 
+		</div>   
+	</header>	
+</html>
